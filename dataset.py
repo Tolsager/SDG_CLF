dd5c76629f7a927b6ab1f4a51b2ca5ddfe41bb31
RSM-git <74561311+RSM-git@users.noreply.github.com>
2022-02-21 15:43:24 +0100

    Dataset in dataset + short EDA in notebook

---
+++ dataset.py
@@ -0,0 +1,37 @@
+import torch
+import pandas as pd
+from transformers import RobertaForSequenceClassification, AutoTokenizer
+from torch.utils.data import Dataset, DataLoader
+import os
+
+class DS(Dataset):
+    def __init__(self, df, tokenizer):
+        self.df = df
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        tweet = self.df.iloc[idx, 2]
+        labels = self.df.iloc[idx, 4:-2]
+        type = self.df.iloc[idx, -2]
+        num_labels = self.df.iloc[idx, -1]
+
+        encoding = self.tokenizer.encode_plus(tweet, padding='max_length', max_length=252, return_tensors='pt', truncation=True)
+
+        ids = torch.squeeze(encoding['input_ids'])
+        mask = torch.squeeze(encoding['attention_mask'])
+
+        return {'input_ids': ids.long(), 'attention_mask': mask.int(), 'labels': torch.tensor(labels), "num_labels": num_labels, 'type': type}
+
+if __name__ == '__main__':
+    if not os.path.exists(os.getcwd() + '\pretrained'):
+        os.mkdir(os.getcwd() + '\pretrained')
+    tokenizer = AutoTokenizer.from_pretrained('roberta-base')
+    tokenizer.save_pretrained('./pretrained/')
+    testing = DS(pd.read_csv('allSDGtweets.csv', encoding='latin1'), tokenizer)
+    testing_dl = DataLoader(testing, batch_size=10)
+
+    for batch in testing_dl:
+        print(batch)

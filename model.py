d54d8c3705f61a6a94ae658436ee9054eb8b5627
RSM-git <rasmus.steen.mikkelsen@hotmail.com>
2022-04-04 18:20:41 +0200

    Sigmoid and scopus EDA start

--- model.py
+++ model.py
@@ -1,8 +1,22 @@
 from transformers import RobertaForSequenceClassification
+import torch.nn as nn
 
-
-def get_model(num_labels=17, pretrained_path: str=None):
+def get_model(num_labels=17, pretrained_path: str=None, multi_class: bool = False):
     if pretrained_path is None:
         pretrained_path = 'roberta-base'
-    model = RobertaForSequenceClassification.from_pretrained(pretrained_path, num_labels=num_labels)
+    if multi_class:
+        class MultiClassModel(nn.Module):
+            def __init__(self, pretrained_path: str = None, num_labels: int = 17):
+                super().__init__()
+                self.roberta = RobertaForSequenceClassification.from_pretrained(pretrained_path, num_labels=num_labels)
+
+                self.sigmoid = nn.Sigmoid()
+
+            def forward(self, x):
+                x = self.roberta(x)
+                print(x)
+                return self.sigmoid(x)
+        model = MultiClassModel(pretrained_path, num_labels)
+    else:
+        model = RobertaForSequenceClassification.from_pretrained(pretrained_path, num_labels=num_labels)
     return model

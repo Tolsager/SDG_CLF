6a38c54e7fbdfd2efafb7059191f440727a12f7a
Rebekka-M <73647490+Rebekka-M@users.noreply.github.com>
2022-05-04 15:34:44 +0200

    Scopus updates

---
+++ scopus_dataset.py
@@ -0,0 +1,99 @@
+import torch
+import pandas as pd
+import transformers
+import os
+import re
+import datasets
+
+from tweet_dataset import preprocess
+
+"""Ideas for splitting the Scopus data into smaller sentences
+    1. Use the average (or max) length of the tweets and split regardless of sentence ending
+    2. Use the max character length of a tweet and round down to work with full words (280 characters)
+    3. 
+
+
+Process:
+    1. Tokenize
+    2. 
+
+model.forward_scopus(input_ids):
+    split input ids into chunks
+    prepend chunk with [CLS]
+    append chunk with [EOS]
+    pad chunk
+
+    for every chunk:
+        preds.append(model(chunk))
+    
+    combine prediction and output final predictions
+"""
+
+
+def load_abstracts(
+    file: str = "data/raw/scopus_ready_to_use.csv",
+    seed: int = 0,
+    nrows: int = None,
+    multi_label: bool = True,
+    tokenizer_type: str = "roberta-base",
+):
+    """Loads the abstracts CSV into a huggingface dataset and apply the preprocessing
+
+    Args:
+        file (str, optional): path to csv file. Defaults to "data/raw/allSDGtweets.csv".
+        seed (int, optional): seed used for shuffling. Defaults to 0.
+
+    Returns:
+        datasets.Dataset: a preprocessed dataset
+    """
+    # load the csv file into a huggingface dataset
+    # Set the encodign to latin to be able to read special characters such as ñ
+    abstracts_df = pd.read_csv(file, encoding="latin", nrows=nrows)
+
+    abstracts_df = abstracts_df.drop_duplicates("text")
+    abstracts_dataset = datasets.Dataset.from_pandas(abstracts_df)
+    if not multi_label:
+        abstracts_dataset = abstracts_dataset.filter(
+            lambda sample: sample["nclasses"] == 1
+        )
+
+    # remove unused columns
+    abstracts_dataset = abstracts_dataset.remove_columns(
+        ["Title", "Year", "Link", "Author.Keywords", "Index.Keywords", "EID", "text"]
+    )
+
+    # Chunking into
+    print(f"Length of dataset before chunking: {tweet_dataset.num_rows}")
+
+    # apply the preprocessing function to every sample
+    tokenizer_path = "tokenizers/" + tokenizer_type.replace("-", "_")
+    if not os.path.exists(tokenizer_path):
+        tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_type)
+        os.makedirs(tokenizer_path)
+        tokenizer.save_pretrained(tokenizer_path)
+    else:
+        tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_type)
+    tweet_dataset = tweet_dataset.map(
+        preprocess,
+        num_proc=6,
+        fn_kwargs={"tokenizer": tokenizer, "tweet": False, "textname": "Abstract"},
+    )
+
+    # UPDATE LATER?
+    tweet_dataset = tweet_dataset.shuffle(seed=seed)
+
+    # tweet_dataset = tweet_dataset.cast_column("label", datasets.Sequence(datasets.Value("float32")))
+    tweet_dataset = tweet_dataset.train_test_split(test_size=0.1)
+    return tweet_dataset
+
+
+if __name__ == "__main__":
+    abstracts_dataset = load_abstracts(nrows=10)
+    # tweet_dataset = load_dataset(nrows=10, multi_label=False)
+    # tweet_dataset = load_dataset()
+    abstracts_dataset.set_format(
+        "torch", columns=["input_ids", "label", "attention_mask"]
+    )
+    # print(tweet_dataset)
+    print(type(abstracts_dataset["train"][0]["input_ids"]))
+    print()
--- tweet_dataset.py
+++ tweet_dataset.py
@@ -23,32 +23,45 @@ def remove_with_regex(sample: dict, pattern: re.Pattern = None):
     return sample
 
 
-def preprocess(sample: dict, tokenizer: transformers.PreTrainedTokenizer):
+def preprocess(
+    sample: dict, tokenizer: transformers.PreTrainedTokenizer, tweet: bool = True, textname: str = 'text'
+):
     """preprocess a sample of the dataset
 
     Args:
         sample (dict): dataset sample
         tokenizer (transformers.PreTrainedTokenizer): a pretrained tokenizer
+        tweet (bool): whether the data are tweets or abstracts
+        textname (str): name of text column in 
 
     Returns:
         dict: the preprocessed sample
     """
-    sample["text"] = sample["text"].lower()
+    sample[textname] = sample[textname].lower()
 
     # remove labels from the tweet
-    sdg_prog = re.compile(r"#(sdg)s?(\s+)?(\d+)?")
-    sample = remove_with_regex(sample, pattern=sdg_prog)
+    sdg_prog1 = re.compile(r"#(sdg)s?(\s+)?(\d+)?")
+    sample = remove_with_regex(sample, pattern=sdg_prog1)
+    sdg_prog2 = re.compile(r"(sdg)s?(\s?)(\d+)?")
+    sample = remove_with_regex(sample, pattern=sdg_prog2)
+    sdg_prog3 = re.compile(r"(sustainable development goals?\s?)(\d+)?")
+    sample = remove_with_regex(sample, pattern=sdg_prog3)
+    sdg_prog4 = re.compile(r"(© \d\d(\d?)\d)?\s")
+    sample = remove_with_regex(sample, pattern=sdg_prog4)
+    sdg_prog5 = re.compile(r"(Elsevier\sLtd)")
+    sample = remove_with_regex(sample, pattern=sdg_prog5)
 
     # remove ekstra whitespace
-    sample["text"] = " ".join(sample["text"].split())
+    sample[textname] = " ".join(sample[textname].split())
 
-    # create a label vector
-    label = [int(sample[f"#sdg{i}"]) for i in range(1, 18)]
-    sample["label"] = label
+    # create a label vector (only applicable for tweets)
+    if tweet:
+        label = [int(sample[f"#sdg{i}"]) for i in range(1, 18)]
+        sample["label"] = label
 
     # tokenize text
     encoding = tokenizer(
-        sample["text"], max_length=260, padding="max_length", truncation=True
+        sample[textname], max_length=260, padding="max_length", truncation=True
     )
     sample["input_ids"] = encoding.input_ids
     sample["attention_mask"] = encoding.attention_mask
@@ -61,12 +74,14 @@ def load_dataset(
     nrows: int = None,
     multi_label: bool = True,
     tokenizer_type: str = "roberta-base",
+    tweet: bool = True,
 ):
     """Loads the tweet CSV into a huggingface dataset and apply the preprocessing
 
     Args:
         file (str, optional): path to csv file. Defaults to "data/raw/allSDGtweets.csv".
         seed (int, optional): seed used for shuffling. Defaults to 0.
+        tweet (bool): whether the data are tweets or abstracts
 
     Returns:
         datasets.Dataset: a preprocessed dataset

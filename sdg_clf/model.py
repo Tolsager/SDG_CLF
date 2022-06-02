2f9c95682ce6b1b8af22bdc4c91773caa0822d66
RSM-git <rasmus.steen.mikkelsen@hotmail.com>
2022-06-02 15:36:58 +0200

    variable amount of layers after transformer structure to build classification head

--- sdg_clf/model.py
+++ sdg_clf/model.py
@@ -3,16 +3,56 @@
 from transformers import (
     AutoModelForSequenceClassification,
     RobertaForSequenceClassification,
+    AutoModel,  
 )
 
 # Why are we installing these?!
 import torch.nn as nn
 import transformers
 
+class Model(nn.Module):
+    def __init__(self, path, n_layers=0, n_labels=17, hidden_size=512, batch_size=8, transformer_hidden_size=768):
+        super().__init__()
+        assert n_layers >= 0
+
+
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.path = path
+
+        self.transformer = AutoModel.from_pretrained(
+            self.path,
+        )
+        self.transformer_o = nn.Sequential(nn.Linear(batch_size*transformer_hidden_size, self.hidden_size),
+                                           nn.ReLU(),
+                                           )
+
+        self.linear = nn.Sequential(nn.Linear(self.hidden_size, self.hidden_size),
+                                    nn.ReLU(),
+                                    )
+
+        self.block = nn.ModuleList([self.transformer_o] + [self.linear for _ in range(n_layers)]) if n_layers > 0 else None
+
+        self.head = nn.Linear(self.hidden_size, self.n_labels) if n_layers > 0 else nn.Linear(transformer_hidden_size*batch_size, self.n_labels)
+    
+    def forward(self, iids, amask):
+        transformer_features = self.transformer(input_ids=iids, attention_mask=amask)[1]
+        transformer_features = transformer_features[:,0,:].view(-1, 768)
+        linear_features = self.block(transformer_features) if self.block is not None else transformer_features
+
+        pred = self.head(linear_features)
+
+        return pred
+
+
 
 def get_model(
     num_labels=17,
     pretrained_path: str = None,
+    n_layers: int = 3,
+    hidden_size: int = 768,
+    batch_size: int = 8,
+    transformer_hidden_size: int = 768,
 ):
     """Function calling the from_pretrained method on Huggingface hosted pretrained models
 
@@ -23,7 +63,5 @@ def get_model(
     Returns:
         _type_: _description_
     """
-    model = AutoModelForSequenceClassification.from_pretrained(
-        pretrained_path, num_labels=num_labels
-    )
+    model = Model(path=pretrained_path, n_layers=n_layers, n_labels=num_labels, hidden_size=hidden_size, batch_size, transformer_hidden_size=transformer_hidden_size)
     return model

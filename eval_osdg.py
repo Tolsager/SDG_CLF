05cf633141150b3af08ab386e4334cb3a7e937e9
Tolsager <victortolsagerolesen1@gmail.com>
2022-06-07 08:52:32 +0200

    add test param to eval_osdg

--- eval_osdg.py
+++ eval_osdg.py
@@ -1,7 +1,6 @@
 import requests
 from osdg_ip import ip1, ip2
 import datasets
-import torchmetrics
 from tqdm import tqdm
 import torch
 import ast
@@ -9,7 +8,7 @@ import pickle
 import torchmetrics
 
 
-def get_scopus_predictions(api: str, split: str = "train"):
+def get_scopus_predictions(api: str, split: str = "train", test: bool = False):
     ds_dict = datasets.load_from_disk("data/processed/scopus/base")
     ds = ds_dict[split]
     predictions = []
@@ -17,14 +16,16 @@ def get_scopus_predictions(api: str, split: str = "train"):
         ip = ip1
     elif api == "second":
         ip = ip2
-    for sample in ds:
+    if test:
+        ds = ds.select([0, 1])
+    for sample in tqdm(ds):
         text = sample["Abstract"]
         data = {"query": text}
         prediction = requests.post(ip, data=data)
         prediction = prediction.text
         prediction = ast.literal_eval(prediction)
         prediction_list = [0] * 17
-        for pred in tqdm(prediction):
+        for pred in prediction:
             sdg_pred = int(pred[0][4:]) - 1
             prediction_list[sdg_pred] = 1
         predictions.append(prediction_list)
@@ -32,9 +33,11 @@ def get_scopus_predictions(api: str, split: str = "train"):
         pickle.dump(predictions, f)
 
 
-def score_predictions(metrics: dict, path_predictions: str):
+def score_predictions(metrics: dict, path_predictions: str, test: bool = False):
     ds_dict = datasets.load_from_disk("data/processed/scopus/base")
     ds = ds_dict["train"]
+    if test:
+        ds = ds.select([0, 1])
     predictions = pickle.load(path_predictions)
     predictions = torch.tensor(predictions)
     labels = ds["label"]
@@ -62,4 +65,6 @@ if __name__ == "__main__":
         "f1":
             torchmetrics.F1Score(num_classes=17, multiclass=not multilabel),
     }
-    results = score_predictions(metrics, "osdg_predictions")
+    with open("osdg_predictions", "rb") as f:
+        results = score_predictions(metrics, f, test=True)
+    print(results)

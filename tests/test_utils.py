6276cd94db5c4c90f48cdb16332a27dd4d96de88
Tolsager <victortolsagerolesen1@gmail.com>
2022-07-01 14:38:20 +0200

    added test

---
+++ tests/test_utils.py
@@ -0,0 +1,7 @@
+from sdg_clf.utils import prepare_long_text_input
+
+# def test_prepare_long_text_input():
+    # input_is = 
+    # tokenizer =
+    # out = prepare_long_text_input(input_is, tokenizer)
+    # assert out == expected_out

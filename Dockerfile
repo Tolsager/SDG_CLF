639a0548cbbe4526a7b99a303a3c07d602fb03fd
Victor Tolsager Olesen <victortolsagerolesen1@gmail.com>
2022-08-19 15:18:07 +0200

    switch to pydantic model input in api and let docker download onnx model from google drive

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,9 @@ RUN pip install --no-cache-dir  -r /code/requirements.txt
 
 COPY . /code
 
+RUN pip install --no-cache-dir gdown \
+    && cd /code/finetuned_models \
+    && gdown https://drive.google.com/uc?id=1qNjTCwMG9Km7Iiy3tLvayLPC6WKloMMj \
+    && cd /code
+
 CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "80"]
--- api.py
+++ api.py
@@ -1,8 +1,11 @@
+import os
+
 import fastapi
+import psutil
+import pydantic
+
 import predict_onnx
 from sdg_clf import modelling, utils
-import os
-import psutil
 
 os.environ["OMP_NUM_THREADS"] = f"{psutil.cpu_count()}"
 os.environ["OMP_WAIT_POLICY"] = "ACTIVE"
@@ -13,9 +16,13 @@ onnx_model = modelling.create_model_for_provider("finetuned_models/roberta-large
 tokenizer = utils.get_tokenizer("roberta-large")
 
 
+class SDGText(pydantic.BaseModel):
+    text: str
+
+
 @app.post("/predict")
-def predict(text: str):
-    prediction = predict_onnx.main(text, onnx_model, tokenizer)
+def predict(SDGText: SDGText):
+    prediction = predict_onnx.main(SDGText.text, onnx_model, tokenizer)
     return {"prediction": prediction}
 
 
